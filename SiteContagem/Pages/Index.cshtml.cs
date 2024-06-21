7800f70a850bf8d4466cad309cb2c8e27b4c3812
Edilson Monteiro <51175821+edymonte@users.noreply.github.com>
2024-06-21 20:22:59 -0300

    Update Index.cshtml.cs

--- SiteContagem/Pages/Index.cshtml.cs
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -21,7 +21,7 @@ namespace SiteContagem.Pages
                 TempData["Local"] = _CONTADOR.Local;
                 TempData["Kernel"] = _CONTADOR.Kernel;
                 TempData["TargetFramework"] = _CONTADOR.TargetFramework;
-                TempData["MensagemFixa"] = "Teste";
+                TempData["MensagemFixa"] = "Teste 2";
                 TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
             }            
         }
